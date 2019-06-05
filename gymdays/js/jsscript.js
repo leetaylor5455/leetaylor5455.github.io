b427a49b944620fd9636d37517edf40da40f5c95
Lee Taylor <leetaylor5455@yahoo.co.uk>
2019-06-05 22:39:07 +0100

    GymDays App Update

--- gymdays/js/jsscript.js
+++ gymdays/js/jsscript.js
@@ -13,36 +13,53 @@ $(document).ready(function() {
   var tomorrowFullDate = new Date(new Date().getTime() + (24 * 60 * 60 * 1000));
   var tomorrowDay = String(tomorrowFullDate).substring(0, 3);
 
-  $.get('js/json/days.json', function(data) {
-    exerciseData = data;
-  }).then(function() {
+  function loadData(set) {
+    $.get('js/json/' + set + '.json', function(data) {
+      exerciseData = data;
+    }).then(function() {
+  
+      function populateHTML(id, day) {
+        jqueryId = '#' + id;
 
-    function populateHTML(id, day) {
-      jqueryId = '#' + id;
-      $(jqueryId).find('.exercise').text(exerciseData[day].exercise);
-
-      var $exercises = $(jqueryId).find('.exercises');
-
-      if (exerciseData[day].exercises.length > 0) {
-        for (var i = 0; i <= exerciseData[day].exercises.length - 1; i++) {
-
-          var exerciseTitle = document.createElement('h3');
-          exerciseTitle.appendChild(document.createTextNode(exerciseData[day].exercises[i][0]));
-
-          var exerciseContent = document.createElement('li');
-          exerciseContent.appendChild(document.createTextNode(exerciseData[day].exercises[i][1]));
-
-          $exercises[0].appendChild(exerciseTitle);
-          $exercises[0].appendChild(exerciseContent);
-        }
+        // Clears Previous Data and Inserts New
+        $(jqueryId).find('.group').text('');
+        $(jqueryId).find('.group').text(exerciseData[day].group);
+  
+        var $exercises = $(jqueryId).find('.exercises');
+        while ($exercises[0].firstChild) {
+          $exercises[0].removeChild($exercises[0].firstChild);
       }
+  
+        if (exerciseData[day].exercises.length > 0) {
+          for (var i = 0; i <= exerciseData[day].exercises.length - 1; i++) {
+  
+            var exerciseTitle = document.createElement('h3');
+            exerciseTitle.appendChild(document.createTextNode(exerciseData[day].exercises[i][0]));
+  
+            var exerciseContent = document.createElement('li');
+            exerciseContent.appendChild(document.createTextNode(exerciseData[day].exercises[i][1]));
+  
+            $exercises[0].appendChild(exerciseTitle);
+            $exercises[0].appendChild(exerciseContent);
+          }
+        }
+  
+  
+      }
+  
+      populateHTML('today', todayDay);
+      populateHTML('tomorrow', tomorrowDay);
+  
+    });
+  }
 
+  loadData('set1');
 
-    }
+  $('#selectSet').change(function(){
+    var set = $(this).val();
+    loadData(set);
+  })
 
-    populateHTML('today', todayDay);
-    populateHTML('tomorrow', tomorrowDay);
-
-  });
+  
 
 });
